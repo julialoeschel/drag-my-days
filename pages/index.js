3d32c486fdc6d3a8049a74412c7f781118fa921a
Julia Löschel <juloeschel@gmail.com>
2022-08-08 22:06:29 +0200

    make everything hotpink

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ import { Item } from "../public/Components/SortableItem";
 export default function Home() {
   const [items, setItems] = useState(data());
   const [activeId, setActiveId] = useState();
+  const [showLecture, setShowLecture] = useState("session");
   const days = Object.keys(items).filter((key) => key.includes("day"));
 
   const sensors = useSensors(
@@ -26,7 +27,6 @@ export default function Home() {
       coordinateGetter: sortableKeyboardCoordinates,
     })
   );
-
   return (
     <section className={styles.container}>
       <DndContext
@@ -43,11 +43,36 @@ export default function Home() {
         </div>
 
         <div className={styles.lowerContainer}>
-          <Container id="sessions" items={items.sessions} />
-          <Container id="guestSessions" items={items.guestSessions} />
-          <Container id="breather" items={items.breather} />
+          <div className={styles.buttonContainer}>
+            <button
+              onClick={() => setShowLecture("session")}
+              className={showLecture === "session" ? styles.activeButton : null}
+            >
+              Sessions
+            </button>
+            <button
+              onClick={() => setShowLecture("guestSession")}
+              className={
+                showLecture === "guestSession" ? styles.activeButton : null
+              }
+            >
+              GuestSession
+            </button>
+            <button
+              onClick={() => setShowLecture("breather")}
+              className={
+                showLecture === "breather" ? styles.activeButton : null
+              }
+            >
+              Breather
+            </button>
+          </div>
+          {showLecture === "session" ? (
+            <Container id="sessions" items={items.sessions} />
+          ) : <Container id="guestSessions" items={items.guestSessions} /> ? (
+            <Container id="breather" items={items.breather} />
+          ) : null}
         </div>
-
         <DragOverlay>{activeId ? <Item id={activeId} /> : null}</DragOverlay>
       </DndContext>
     </section>
